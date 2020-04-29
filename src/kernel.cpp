aeaa4b84868c3237f0066899d54970f2e748f981
ruben beneden <rubenebruin@gmail.com>
2020-04-29 15:47:51 +0200

    enabled line-123 and seg-seg

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -676,8 +676,8 @@ void init_skgeom_kernel(py::module &m) {
 
 
     m.def("do_intersect", &do_intersect<Line_3, Line_3>);
-    //m.def("do_intersect", &do_intersect<Line_3, Plane_3>);
-    //m.def("do_intersect", &do_intersect<Line_3, Ray_3>);
+    m.def("do_intersect", &do_intersect<Line_3, Plane_3>);
+    m.def("do_intersect", &do_intersect<Line_3, Ray_3>);
     //m.def("do_intersect", &do_intersect<Line_3, Segment_3>);
     //m.def("do_intersect", &do_intersect<Line_3, Triangle_3>);
 
