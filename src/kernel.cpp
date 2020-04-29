00826ca6fa2aa274ba8e18a32a262b877f2227ea
ruben beneden <rubenebruin@gmail.com>
2020-04-29 12:05:27 +0200

    testing

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -675,27 +675,27 @@ void init_skgeom_kernel(py::module &m) {
     
 
 
-    m.def("do_intersect", &do_intersect<Line_3, Line_3>);
-    m.def("do_intersect", &do_intersect<Line_3, Plane_3>);
-    m.def("do_intersect", &do_intersect<Line_3, Ray_3>);
-    m.def("do_intersect", &do_intersect<Line_3, Segment_3>);
-    m.def("do_intersect", &do_intersect<Line_3, Triangle_3>);
+    // m.def("do_intersect", &do_intersect<Line_3, Line_3>);
+    // m.def("do_intersect", &do_intersect<Line_3, Plane_3>);
+    // m.def("do_intersect", &do_intersect<Line_3, Ray_3>);
+    // m.def("do_intersect", &do_intersect<Line_3, Segment_3>);
+    // m.def("do_intersect", &do_intersect<Line_3, Triangle_3>);
 
-    m.def("do_intersect", &do_intersect<Plane_3, Plane_3>);
-    m.def("do_intersect", &do_intersect<Plane_3, Ray_3>);
-    m.def("do_intersect", &do_intersect<Plane_3, Segment_3>);
-    m.def("do_intersect", &do_intersect<Plane_3, Sphere_3>);
-    m.def("do_intersect", &do_intersect<Plane_3, Triangle_3>);
+    // m.def("do_intersect", &do_intersect<Plane_3, Plane_3>);
+    // m.def("do_intersect", &do_intersect<Plane_3, Ray_3>);
+    // m.def("do_intersect", &do_intersect<Plane_3, Segment_3>);
+    // m.def("do_intersect", &do_intersect<Plane_3, Sphere_3>);
+    // m.def("do_intersect", &do_intersect<Plane_3, Triangle_3>);
 
-    m.def("do_intersect", &do_intersect<Ray_3, Ray_3>);
-    m.def("do_intersect", &do_intersect<Ray_3, Segment_3>);
-    m.def("do_intersect", &do_intersect<Ray_3, Triangle_3>);
+    // m.def("do_intersect", &do_intersect<Ray_3, Ray_3>);
+    // m.def("do_intersect", &do_intersect<Ray_3, Segment_3>);
+    // m.def("do_intersect", &do_intersect<Ray_3, Triangle_3>);
 
-    m.def("do_intersect", &do_intersect<Segment_3, Segment_3>);
-    m.def("do_intersect", &do_intersect<Segment_3, Triangle_3>);
+    // m.def("do_intersect", &do_intersect<Segment_3, Segment_3>);
+    // m.def("do_intersect", &do_intersect<Segment_3, Triangle_3>);
 
-    m.def("do_intersect", &do_intersect<Sphere_3, Sphere_3>);
-    m.def("do_intersect", &do_intersect<Triangle_3, Triangle_3>);
+    // m.def("do_intersect", &do_intersect<Sphere_3, Sphere_3>);
+    // m.def("do_intersect", &do_intersect<Triangle_3, Triangle_3>);
 
 
     // intersection, same combinations
