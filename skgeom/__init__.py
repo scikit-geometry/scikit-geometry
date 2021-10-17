b77fa0745e11bcfe851082339e4bbd75b113d716
Tim Savage <tim@savage.company>
2021-10-17 23:04:55 +1100

    Make draw optional removing the requirements of installing matplotlib

--- skgeom/__init__.py
+++ skgeom/__init__.py
@@ -1,6 +1,9 @@
 # import all contents from C++ extension module
 from ._skgeom import *
 
-from . import draw
+try:
+    from . import draw
+except ImportError:
+    pass
 
 from ._version import version_info, __version__
