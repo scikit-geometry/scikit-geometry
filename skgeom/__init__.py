dfcaec1eb8c70488293e49127e4d97008bb15de6
Tim Savage <tim@savage.company>
2021-10-18 17:50:15 +1100

    Add warning if matplotlib cannot be imported

--- skgeom/__init__.py
+++ skgeom/__init__.py
@@ -4,6 +4,9 @@ from ._skgeom import *
 try:
     from . import draw
 except ImportError:
-    pass
+    import warnings
+    warnings.warn(
+        "The draw module requires the `matplotlib` package to be installed"
+    )
 
 from ._version import version_info, __version__
