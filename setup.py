0357ba8430b53c7e112691549f3bea7b0de11a69
Tim Savage <tim@savage.company>
2021-10-18 18:59:53 +1100

    Add matplotlib as an optional dependency, add numpy as a required dependency

--- setup.py
+++ setup.py
@@ -223,8 +223,11 @@ setup(
     description='scikit-geometry, the python computational geometry library',
     long_description='',
     ext_modules=ext_modules,
-    install_requires=['pybind11>=2.3,<2.8'],
+    install_requires=['pybind11>=2.3,<2.8', 'numpy'],
     setup_requires=['pybind11>=2.3,<2.8'],
+    extras_require={
+        "drawing": ["matplotlib"],
+    },
     cmdclass={'build_ext': BuildExt},
     zip_safe=False,
     packages=['skgeom'],
