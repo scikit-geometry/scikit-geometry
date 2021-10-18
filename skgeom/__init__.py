2ec6387bbcfa6d69a8009543e9aaa520c3859eb7
Tim Savage <tim@savage.company>
2021-10-18 19:02:40 +1100

    Move draw import error into the skgeom.draw module

--- skgeom/__init__.py
+++ skgeom/__init__.py
@@ -1,12 +1,4 @@
 # import all contents from C++ extension module
 from ._skgeom import *
 
-try:
-    from . import draw
-except ImportError:
-    import warnings
-    warnings.warn(
-        "The draw module requires the `matplotlib` package to be installed"
-    )
-
 from ._version import version_info, __version__
