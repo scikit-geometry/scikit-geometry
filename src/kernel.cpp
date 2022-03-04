c870a982f99e2437453d4076e766fed662f883d3
Joshua Skrzypek <jskrzypek@gmail.com>
2022-03-04 16:09:50 -0500

    fix: Convert comments to ifndefs
    Instead of commenting out implmentations of do_intersect in kernel.cpp that fail
    to compile on osx, wrap them in #ifndef __APPLE__ and still be able to build on
    macs.

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -672,7 +672,9 @@ void init_skgeom_kernel(py::module &m) {
     m.def("do_intersect", &do_intersect<Line_3, Line_3>);
     m.def("do_intersect", &do_intersect<Line_3, Plane_3>);
     m.def("do_intersect", &do_intersect<Line_3, Ray_3>);
-    //m.def("do_intersect", &do_intersect<Line_3, Segment_3>); // <-- THIS ONE ERRORS ON OS-X, windows (VS) & linux work fine
+#ifndef __APPLE__
+    m.def("do_intersect", &do_intersect<Line_3, Segment_3>); // <-- THIS ONE ERRORS ON OS-X, windows (VS) & linux work fine
+#endif
     m.def("do_intersect", &do_intersect<Line_3, Triangle_3>);
 
     m.def("do_intersect", &do_intersect<Plane_3, Plane_3>);
@@ -683,10 +685,14 @@ void init_skgeom_kernel(py::module &m) {
 
     
     m.def("do_intersect", &do_intersect<Ray_3, Ray_3>);
-    //m.def("do_intersect", &do_intersect<Ray_3, Segment_3>); // <-- THIS ONE ERRORS ON OS-X, windows (VS) & linux work fine
+#ifndef __APPLE__
+    m.def("do_intersect", &do_intersect<Ray_3, Segment_3>); // <-- THIS ONE ERRORS ON OS-X, windows (VS) & linux work fine
+#endif
     m.def("do_intersect", &do_intersect<Ray_3, Triangle_3>);
 
-    // m.def("do_intersect", &do_intersect<Segment_3, Segment_3>); // <-- THIS ONE ERRORS ON OS-X, windows (VS) & linux work fine
+#ifndef __APPLE__
+    m.def("do_intersect", &do_intersect<Segment_3, Segment_3>); // <-- THIS ONE ERRORS ON OS-X, windows (VS) & linux work fine
+#endif
     
     m.def("do_intersect", &do_intersect<Segment_3, Triangle_3>);
     m.def("do_intersect", &do_intersect<Sphere_3, Sphere_3>);
